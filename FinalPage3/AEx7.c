f43a1ff86c30a61b9e2ad8efb6ab5e5e97813c02
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-07-01 05:13:31 +0300

    fix

--- FinalPage3/AEx7.c
+++ FinalPage3/AEx7.c
@@ -1,13 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-//void revers(int a[], int size) {
-//	int i;
-//	for (i = 0; i < size / 2; i++)
-//	{
-//		swap(a[i], a[size - 1 - i]);
-//	}
-//}
+void revers(int a[], int size) {
+	int i;
+	for (i = 0; i < size / 2; i++)
+	{
+		swap(a[i], a[size - 1 - i]);
+	}
+}
 
 void flip(int a[], int m, int n) {
 	int i, j, temp;
@@ -20,15 +20,4 @@ void flip(int a[], int m, int n) {
 		}
 		a[0] = temp;
 	}
-
-	for (i = 0; i < m + n; i++)
-	{
-		printf(" %d", a[i]);
-	}
-}
-
-int main() {
-	int a[6] = { 1,5, 0, 2 , 8, 3};
-	flip(a ,2, 4);
-	return 0;
 }
